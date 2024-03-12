90eb1d8533fc573f1e4d72dc759a863f94561a65
denniskioko <kiokodennis6990@gmail.com>
2024-03-12 08:23:21 +0300

    Completed assignment

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,45 +1,28 @@
 class Pet:
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-    all_pets = []
+    all = []
 
     def __init__(self, name, pet_type, owner=None):
+
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet".format(self.PET_TYPES))
+        self.pet_type = pet_type
         self.name = name
         self.owner = owner
-        self.pet_type = pet_type
-        if pet_type not in self.PET_TYPES:
-            raise Exception("Invalid pet type")
-        Pet.all_pets.append(self)
+        Pet.all.append(self)
 
 
 class Owner:
     def __init__(self, name):
         self.name = name
-        self.pets_owned = []
 
     def pets(self):
-        return self.pets_owned
+        return [pet for pet in Pet.all]
 
     def add_pet(self, pet):
         if not isinstance(pet, Pet):
-            raise TypeError("pet unavailable")
+            raise Exception("Not a Pet instance")
         pet.owner = self
-        self.pets_owned.append(pet)
-    
+
     def get_sorted_pets(self):
-        return sorted(self.pets_owned, key=lambda x: x.name)
-
-# Testing the code
-owner = Owner("John")
-pet1 = Pet("Fido", "dog", owner)
-pet2 = Pet("Whiskers", "cat", owner)
-pet3 = Pet("Rex", "dog")
-
-for pet in owner.get_sorted_pets():
-    print(pet.name, "-", pet.pet_type)
-
-def test_pet_has_all():
-    """Test Pet class has variable all, storing all instances of Pet"""
-    pet1 = Pet("Whiskers", "cat")
-    pet2 = Pet("Jerry", "reptile")
-
-    assert pet1 in Pet.all_pets
+        return sorted(self.pets(), key=lambda pet: pet.name)
